36a554c6d7d2baa3b89913b1b86376a981431788
ljc <1195542815@qq.com>
2024-12-10 21:07:48 +0800

    add seal

--- src/views/signSeal/AuthenticationDialog/index.vue
+++ src/views/signSeal/AuthenticationDialog/index.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
-
-type table = {
-  cols: TableColumnList;
-  tableData: any[];
-  total: number;
-};
-
-const props = defineProps<table>();
+import { h, reactive } from "vue";
+import { tableData } from "@/views/table/base/data";
+import Btn from "@/views/signSeal/SignManage/btn/index.vue";
+const columns: TableColumnList = [
+  {
+    label: "文档名称",
+    prop: "date"
+  },
+  {
+    label: "发起时间",
+    prop: "name"
+  },
+  {
+    label: "文档状态",
+    prop: "address"
+  },
+  {
+    label: "操作",
+    cellRenderer(data) {
+      return h(Btn, {
+        dwClick: () => {
+          console.log(data);
+        },
+        detailClick: () => {
+          console.log(data);
+        },
+        delClick: () => {
+          console.log(data);
+        }
+      });
+    },
+    align: "center"
+  }
+];
 </script>
 
 <template>
   <div>
-    {{ tableData }}
-    <pure-table :data="table.tableData" :col="table.cols"></pure-table>
+    <pure-table style="margin-top: 20px" :data="tableData" :columns="columns" />
+    <el-row style="margin-top: 20px; justify-content: flex-end; width: 100%">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000"
+        @current-change="currentChange"
+      />
+    </el-row>
   </div>
 </template>
 
